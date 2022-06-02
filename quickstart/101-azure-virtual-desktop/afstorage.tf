9e4b9408da56958cc6073287053dd0ecc2ca1e60
vineet-persad <54596211+vineet-persad@users.noreply.github.com>
2022-06-02 11:28:59 -0400

    Update afstorage.tf

--- quickstart/101-azure-virtual-desktop/afstorage.tf
+++ quickstart/101-azure-virtual-desktop/afstorage.tf
@@ -23,7 +23,19 @@ resource "azurerm_storage_account" "storage" {
   account_replication_type = "LRS"
   account_kind             = "FileStorage"
 }
+}
 
+## Azure Storage Accounts requires a globally unique names
+## https://docs.microsoft.com/en-us/azure/storage/common/storage-account-overview
+## Create a File Storage Account 
+resource "azurerm_storage_account" "storage" {
+  name                     = "stor${random_string.random.id}"
+  resource_group_name      = azurerm_resource_group.rg_storage.name
+  location                 = azurerm_resource_group.rg_storage.location
+  account_tier             = "Premium"
+  account_replication_type = "LRS"
+  account_kind             = "FileStorage"
+}
 resource "azurerm_storage_share" "FSShare" {
   name                 = "fslogix"
   storage_account_name = azurerm_storage_account.storage.name
